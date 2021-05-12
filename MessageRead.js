455052a19c392ba593477a09dccaa11f737d5378
Glen Scales <gscales@martellotech.com>
2021-05-12 13:12:02 +1000

    Initial check-in

---
+++ MessageRead.js
@@ -0,0 +1,194 @@
+﻿    var messageBanner;
+
+    // The Office initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+            ExecuteSearch();
+
+        });
+
+    };
+
+    function ExecuteSearch(){
+        if(Office.context.mailbox.item.sender.emailAddress == "noreply@email.teams.microsoft.com"){
+            resolveName(Office.context.mailbox.item.sender.displayName.replace(" in Teams",""));
+        }else{
+            getTeamsMessagesFolder(Office.context.mailbox.item.sender.emailAddress);
+        }
+    }
+    function resolveName(NameToLookup){
+        var request = GetResolveNameRequest(NameToLookup);
+        var EmailAddress = "";        
+        Office.context.mailbox.makeEwsRequestAsync(request, function (asyncResult) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(asyncResult.value, "text/xml");
+            var values = doc.getElementsByTagName("t:EmailAddress");
+            if(values.length != 0){
+                EmailAddress = values[0].textContent;
+                getTeamsMessagesFolder(EmailAddress);
+            }        
+
+        });
+
+    }
+
+    function getTeamsMessagesFolder(EmailAddress){
+        var request = FindFolderRequest();
+  
+        Office.context.mailbox.makeEwsRequestAsync(request, function (asyncResult) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(asyncResult.value, "text/xml");
+            var folderid = doc.getElementsByTagName("t:FolderId");
+            if(folderid.length != 0){
+                FindItems(folderid[0].getAttribute('Id'),EmailAddress);
+            }        
+
+        });
+
+    }
+
+    function FindItems(FolderId,EmailAddress){
+        var request = FindItemsRequest(FolderId,EmailAddress);  
+        Office.context.mailbox.makeEwsRequestAsync(request, function (asyncResult) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(asyncResult.value, "text/xml");
+            var Items = doc.getElementsByTagName("t:Message");
+            DisplayMessages(Items);
+
+        });
+    }
+
+
+    function base64ToHex(str) {
+        const raw = atob(str);
+        let result = '';
+        for (let i = 0; i < raw.length; i++) {
+          const hex = raw.charCodeAt(i).toString(16);
+          result += (hex.length === 2 ? hex : '0' + hex);
+        }
+        return result.toUpperCase();
+      } 
+
+    function GetResolveNameRequest(NameToLookup) {
+        var results =    
+
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '  <soap:Header>' +
+        '    <t:RequestServerVersion Version="Exchange2013" />' +
+        '  </soap:Header>' +
+        '  <soap:Body>' +
+        '    <m:ResolveNames ReturnFullContactData="true" SearchScope="ActiveDirectory">' +
+        '      <m:UnresolvedEntry>' + NameToLookup + '</m:UnresolvedEntry>' +
+        '    </m:ResolveNames>' +
+        '  </soap:Body>' +
+        '</soap:Envelope>'
+         return results;
+    }
+
+
+    function FindFolderRequest(){
+        var RequestString =
+        '<?xml version="1.0" encoding="utf-8"?>' +
+        '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '  <soap:Header>' +
+        '    <t:RequestServerVersion Version="Exchange2016" />' +
+        '  </soap:Header>' +
+        '  <soap:Body>' +
+        '<m:FindFolder Traversal="Deep">' +
+        '<m:FolderShape>' +
+        '  <t:BaseShape>AllProperties</t:BaseShape>' +
+        '</m:FolderShape>' +
+        '<m:IndexedPageFolderView MaxEntriesReturned="1" Offset="0" BasePoint="Beginning" />' +
+        '<m:Restriction>' +
+        '  <t:IsEqualTo>' +
+        '    <t:FieldURI FieldURI="folder:DisplayName" />' +
+        '    <t:FieldURIOrConstant>' +
+        '      <t:Constant Value="TeamsMeetings" />' +
+        '    </t:FieldURIOrConstant>' +
+        '  </t:IsEqualTo>' +
+        '</m:Restriction>' +
+        '<m:ParentFolderIds>' +
+        '  <t:DistinguishedFolderId Id="root" />' +
+        '</m:ParentFolderIds>' +
+        '</m:FindFolder>' +
+        '  </soap:Body>' +
+        '</soap:Envelope>'
+        return RequestString;
+  
+    }
+
+    function FindItemsRequest(FolderId,EmailAddress) {
+        var RequestString =
+          '<?xml version="1.0" encoding="utf-8"?>' +
+          '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+          '  <soap:Header>' +
+          '    <t:RequestServerVersion Version="Exchange2016" />' +
+          '  </soap:Header>' +
+          '  <soap:Body>' +
+          '<m:FindItem Traversal="Shallow">' +
+          '<m:ItemShape>' +
+          '  <t:BaseShape>IdOnly</t:BaseShape>' +
+          '  <t:AdditionalProperties>' +  
+          '  <t:FieldURI FieldURI="item:ItemClass" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" />' +
+          '  <t:ExtendedFieldURI PropertyTag="96" PropertyType="SystemTime" />' +    
+          '  <t:ExtendedFieldURI PropertyTag="97" PropertyType="SystemTime" />' + 
+          '  <t:ExtendedFieldURI PropertyTag="23809" PropertyType="String" />' + 
+          '  </t:AdditionalProperties>' +  
+          '</m:ItemShape>' +
+          '<m:IndexedPageItemView MaxEntriesReturned="40" Offset="0" BasePoint="Beginning" />' +
+          '<m:ParentFolderIds>' +
+          ' <t:FolderId Id="' + FolderId + '" />' +
+          '</m:ParentFolderIds>' +
+          '<m:QueryString>participants:"' + EmailAddress + '"</m:QueryString>' +
+          '</m:FindItem>' +
+          '  </soap:Body>' +
+          '</soap:Envelope>'
+        return RequestString;
+      }
+    function DisplayMessages(Messages) {
+        try {
+            
+            var html = "<div class=\"ms-Table-row\">";
+            html = html + "<span class=\"ms-Table-cell\"></span>";
+            html = html + "<span class=\"ms-Table-cell\">Details</span>";
+            html = html + "</div>";
+            for (let Message of Messages) {    
+                var TeamsEventType = Message.childNodes[1].textContent.substring(Message.childNodes[1].textContent.lastIndexOf(".") + 1);          
+                var eStartDate = new Date(Message.childNodes[2].textContent);
+                var eEndDate = new Date(Message.childNodes[3].textContent);
+                html = html + "<div class=\"ms-Table-row\">";
+                html = html + "<span class=\"ms-Table-cell\">Type</span>";
+                html = html +"<span class=\"ms-Table-cell\">" + TeamsEventType + "</span>";
+                html = html + "</div><div class=\"ms-Table-row\">";
+                html = html + "<span class=\"ms-Table-cell\">Start Time</span>";
+                html = html + "<span class=\"ms-Table-cell\">" + eStartDate.toString('dd-MMM-yy HH:mm') + "</span>";
+                html = html + "</div><div class=\"ms-Table-row\">";
+                html = html + "<span class=\"ms-Table-cell\">End Time</span>";
+                html = html + "<span class=\"ms-Table-cell\">" + eEndDate.toString('dd-MMM-yy HH:mm') + "</span>";
+                html = html + "</div><div class=\"ms-Table-row\">";
+                if(TeamsEventType == "Meeting"){
+                    html = html + "<span class=\"ms-Table-cell\">Organizer</span>";
+                }else{
+                    html = html + "<span class=\"ms-Table-cell\">Caller</span>";
+                }
+                html = html + "<span class=\"ms-Table-cell\">" +  Message.childNodes[4].textContent + "</span></div >";
+                html = html + "<div class=\"ms-Table-row\"></div>";
+            }
+            $('#mTchatTable').empty().append(html);
+        } catch (error) {
+            $('#mTchatTable').html("Error displaying table " + error);
+        }
+    }
+
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Microsoft Teams Call History Outlook/OWA Addin #
+
+One of the Microsoft Teams features that I like the least is call history, for a number of reasons firstly its quite limited only 30 days worth of data and only includes direct calls and not meetings (which are often just scheduled calls). So when it comes to actually answering a question (like the one I found myself asking last week) when did I last have a call or meeting with person X it can't be used to answer this question. The other thing is having that information available in the context of Outlook/OWA is a lot more useful for me (anyway) then digging through anther client to find its doesn't answer the question anyway. However while the call history in Teams only goes back 30 days the Teams CDR (Call Data Records) which I covered in [https://dev.to/gscales/accessing-microsoft-teams-summary-records-cdr-s-for-calls-and-meetings-using-exchange-web-services-3581](https://www.blogger.com/u/1/blog/post/edit/7123450/8079661183382240070#) do go back as far as your mailbox folder retention period. So with a little work I can use Exchange Web Services to create an Outlook/OWA addin that can be used to show the Call history for a person inline in OWA when you active it on a message you received from that user Eg
+
+![](https://1.bp.blogspot.com/-WJGIYKDZ2QM/YJjCw-AMckI/AAAAAAAAOGo/HcTdu6C4qH497My59jGYaJYe-xWpC2zoACLcBGAsYHQ/w640-h530/callhist1.PNG)
+
+ 
+
+How this works is that the CDR item that is stored in the Mailbox stores the participants of the Call or Meeting in the recipients collection of the CDR mailbox item. The means you can make a KQL query on the participants to find all the CDR items that involve a particular user based on the sender address of the Email that the OWA Addin is activated on. eg the EWS Query ends up looking like
+
+```
+<soap:Envelope
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages"
+	xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+	xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+	<soap:Header>
+		<t:RequestServerVersion Version="Exchange2016" />
+	</soap:Header>
+	<soap:Body>
+		<m:FindItem Traversal="Shallow">
+			<m:ItemShape>
+				<t:BaseShape>IdOnly</t:BaseShape>
+				<t:AdditionalProperties>
+					<t:FieldURI FieldURI="item:ItemClass"
+						xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types" />
+						<t:ExtendedFieldURI PropertyTag="96" PropertyType="SystemTime" />
+						<t:ExtendedFieldURI PropertyTag="97" PropertyType="SystemTime" />
+						<t:ExtendedFieldURI PropertyTag="23809" PropertyType="String" />
+					</t:AdditionalProperties>
+				</m:ItemShape>
+				<m:IndexedPageItemView MaxEntriesReturned="40" Offset="0" BasePoint="Beginning" />
+				<m:ParentFolderIds>
+					<t:FolderId Id="A=....." />
+				</m:ParentFolderIds>
+				<m:QueryString>participants:"e5tmp5@datarumble.com"</m:QueryString>
+			</m:FindItem>
+		</soap:Body>
+	</soap:Envelope>
+```
+
+The extended properties that are used for the Start and End are pidTagStart and pidTagEnd which get set on the CDR mailbox item and then the SenderEmailSMTP address is used to work out who is the Organizer or the call initiator. To work out the type of Call the ItemClass is used so the last part of the ItemClass will tell you what the CRD is for eg call or meeting (If your wondering if you could do the same with the Microsoft Graph the answer is no because the Items in question are not a subclass of IPM.Note so aren't accessible when using the Microsoft Graph)
+
+Want to give it a try yourself ?
+
+I've hosted the files on my GitHub pages so its easy to test (if you like it clone it and host it somewhere else). But all you need to do is add it as a custom addin (if you allowed to) using the 
+URL-
+
+  https://gscales.github.io/TeamsCallHistory/TeamsCallHistory.xml
+
+![](C:\Users\gscal\OneDrive\Pictures\addin3.JPG)
